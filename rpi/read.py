e8aa946228e749995d852feda7d8d494f532c469
David Langheiter <david@langheiter.com>
2019-06-04 21:46:24 +0200

    Added Class to access Sensor

---
+++ rpi/read.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import smbus
+import time
+
+class TSL2561:
+    def __init__(self, integration_time=2, debounce_time=0.5):
+        self.bus = smbus.SMBus(1)
+
+        # Power ON Mode
+        self.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+
+        # Set nominal integration time
+        # 0 = 13.7ms; 1 = 101ms; 2 = 402ms; 3 = manual mode
+        self.write_byte_data(0x39, 0x01 | 0x80, integration_time)
+
+        # Make sure sensor is ready
+        time.sleep(debounce_time)
+
+    def write_byte_data(self, ctrl_reg, cmd_reg, val):
+        return self.bus.write_byte_data(ctrl_reg, cmd_reg, val)
+
+    def read_i2c_block_data(self, block, cmd_reg, bytes):
+        return self.bus.read_i2c_block_data(block, cmd_reg, bytes)
+
+    def get_data(self):
+        # Read data from sensor
+        data1 = self.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+        data2 = self.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+
+        # Convert data
+        full = data1[1] * 256 + data1[0]
+        infrared = data2[1] * 256 + data2[0]
+
+        return full, infrared
+
+if __name__ == '__main__':
+    tsl = TSL2561()
+
+    full, inf = tsl.get_data()
+
+    print("Full Spectrum(IR + Visible) :%d lux" %full)
+    print("Infrared Value :%d lux" %inf)
+    print("Visible Value :%d lux" %(full - inf))
