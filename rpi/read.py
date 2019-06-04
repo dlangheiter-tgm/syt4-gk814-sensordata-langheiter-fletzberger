aa1cee4291cb4196ea78fee24eefec0a129ecd92
David Langheiter <david@langheiter.com>
2019-06-04 21:50:36 +0200

    Made bus_address static

--- rpi/read.py
+++ rpi/read.py
@@ -4,29 +4,31 @@ import smbus
 import time
 
 class TSL2561:
+    bus_address = 0x39
+
     def __init__(self, integration_time=2, debounce_time=0.5):
         self.bus = smbus.SMBus(1)
 
         # Power ON Mode
-        self.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+        self.write_byte_data(0x00 | 0x80, 0x03)
 
         # Set nominal integration time
         # 0 = 13.7ms; 1 = 101ms; 2 = 402ms; 3 = manual mode
-        self.write_byte_data(0x39, 0x01 | 0x80, integration_time)
+        self.write_byte_data(0x01 | 0x80, integration_time)
 
         # Make sure sensor is ready
         time.sleep(debounce_time)
 
-    def write_byte_data(self, ctrl_reg, cmd_reg, val):
-        return self.bus.write_byte_data(ctrl_reg, cmd_reg, val)
+    def write_byte_data(self, cmd_reg, val):
+        return self.bus.write_byte_data(self.bus_address, cmd_reg, val)
 
-    def read_i2c_block_data(self, block, cmd_reg, bytes):
-        return self.bus.read_i2c_block_data(block, cmd_reg, bytes)
+    def read_i2c_block_data(self, cmd_reg, bytes):
+        return self.bus.read_i2c_block_data(self.bus_address, cmd_reg, bytes)
 
     def get_data(self):
         # Read data from sensor
-        data1 = self.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
-        data2 = self.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+        data1 = self.read_i2c_block_data(0x0C | 0x80, 2)
+        data2 = self.read_i2c_block_data(0x0E | 0x80, 2)
 
         # Convert data
         full = data1[1] * 256 + data1[0]
